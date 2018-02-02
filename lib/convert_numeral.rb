4643104dac05b85c4982dc79f74e7835b7d74712
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2018-02-02 19:54:52 +0900

    add to_chinese_numeral to Integer.

--- lib/convert_numeral.rb
+++ lib/convert_numeral.rb
@@ -1,5 +1,72 @@
-require "convert_numeral/version"
+require 'convert_numeral/version'
 
 module ConvertNumeral
-  # Your code goes here...
+end
+
+class Integer
+  TEN_THOUSAND_NUMERALS = [''].concat(%w[万 億 兆 京 垓 𥝱 穣 溝 澗 正 載 極 恒河沙 阿僧祇 那由他 不可思議 無量大数]).map(&:freeze).freeze
+  THOUSAND_NUMERALS = %w(十 百 千).map(&:freeze).freeze
+  NUMERALS = %w(零 一 二 三 四 五 六 七 八 九).map(&:freeze).freeze
+
+  def to_chinese_numeral
+    return NUMERALS[0] if self == 0
+
+    string = self.to_s
+    total_length = string.length
+
+    return convert_thousand(string, total_length) if total_length <= 4
+
+    quo, mod = total_length.divmod(4)
+    limit = (mod == 0) ? quo - 1 : quo
+
+    separated_numerals_by_four_digit =
+      (0..limit).map do |index|
+        if index < limit
+          string[-(index * 4 + 4), 4]
+        elsif index == limit
+          if mod == 0
+            string[-(index * 4 + 4), 4]
+          else
+            string[-(index * 4 + mod), mod]
+          end
+        end
+      end
+
+    separated_numerals_by_four_digit
+      .map.with_index do |numeral, index|
+        converted_string = convert_thousand(numeral, numeral.length, index, limit)
+        converted_string.empty? ? '' : converted_string + TEN_THOUSAND_NUMERALS[index]
+      end
+      .reverse
+      .join
+  end
+
+  private
+
+  def convert_thousand(string, char_count, index = 0, limit = 0)
+    (1..char_count).inject('') do |current_string, digit|
+      numeral = string[-digit].to_i
+      thousand_numeral = THOUSAND_NUMERALS[digit - 2]
+      add_string =
+        if digit == 1
+          numeral > 0 ? NUMERALS[numeral] : ''
+        elsif digit == 4
+          if index < limit
+            numeral > 0 ? NUMERALS[numeral] + thousand_numeral : ''
+          else
+            numeral > 1 ? NUMERALS[numeral] + thousand_numeral : thousand_numeral
+          end
+        else
+          if numeral == 0
+            ''
+          elsif numeral == 1
+            thousand_numeral
+          else
+            NUMERALS[numeral] + thousand_numeral
+          end
+        end
+
+      add_string + current_string
+    end
+  end
 end
--- spec/convert_numeral_spec.rb
+++ spec/convert_numeral_spec.rb
@@ -1,9 +1,71 @@
+require 'spec_helper'
+
 RSpec.describe ConvertNumeral do
-  it "has a version number" do
+  it 'has a version number' do
     expect(ConvertNumeral::VERSION).not_to be nil
   end
+end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+RSpec.describe Integer do
+  describe '#to_chinese_numeral' do
+    it '' do
+      expect(0.to_chinese_numeral).to eq '零'
+      expect(1.to_chinese_numeral).to eq '一'
+      expect(2.to_chinese_numeral).to eq '二'
+      expect(10.to_chinese_numeral).to eq '十'
+      expect(13.to_chinese_numeral).to eq '十三'
+      expect(44.to_chinese_numeral).to eq '四十四'
+      expect(100.to_chinese_numeral).to eq '百'
+      expect(605.to_chinese_numeral).to eq '六百五'
+      expect(617.to_chinese_numeral).to eq '六百十七'
+      expect(728.to_chinese_numeral).to eq '七百二十八'
+      expect(1000.to_chinese_numeral).to eq '千'
+      expect(1001.to_chinese_numeral).to eq '千一'
+      expect(1010.to_chinese_numeral).to eq '千十'
+      expect(1025.to_chinese_numeral).to eq '千二十五'
+      expect(1111.to_chinese_numeral).to eq '千百十一'
+      expect(1321.to_chinese_numeral).to eq '千三百二十一'
+      expect(9999.to_chinese_numeral).to eq '九千九百九十九'
+      expect(1_0000.to_chinese_numeral).to eq '一万'
+      expect(1_0001.to_chinese_numeral).to eq '一万一'
+      expect(1_1111.to_chinese_numeral).to eq '一万一千百十一'
+      expect(20_0030.to_chinese_numeral).to eq '二十万三十'
+      expect(1321_1400.to_chinese_numeral).to eq '千三百二十一万一千四百'
+      expect(4567_8629.to_chinese_numeral).to eq '四千五百六十七万八千六百二十九'
+      expect(1_0000_0000.to_chinese_numeral).to eq '一億'
+      expect(1_0000_1000.to_chinese_numeral).to eq '一億一千'
+      expect(1_1000_5000.to_chinese_numeral).to eq '一億一千万五千'
+      expect(1000_0000_0000.to_chinese_numeral).to eq '千億'
+      expect(1_0000_0000_0000.to_chinese_numeral).to eq '一兆'
+      expect(1000_1000_1000_1000.to_chinese_numeral).to eq '千兆一千億一千万一千'
+      expect(1_0000_0000_0000_0000.to_chinese_numeral).to eq '一京'
+      expect(1000_1000_1000_1000_1000.to_chinese_numeral).to eq '千京一千兆一千億一千万一千'
+      expect(1_0000_0000_0000_0000_0000.to_chinese_numeral).to eq '一垓'
+      expect(1000_1000_1000_1000_1000_1000.to_chinese_numeral).to eq '千垓一千京一千兆一千億一千万一千'
+      expect(1_0000_0000_0000_0000_0000_0000.to_chinese_numeral).to eq '一𥝱'
+      expect(1000_1000_1000_1000_1000_1000_1000.to_chinese_numeral).to eq '千𥝱一千垓一千京一千兆一千億一千万一千'
+      expect(1_0000_0000_0000_0000_0000_0000_0000.to_chinese_numeral).to eq '一穣'
+      expect(1000_1000_1000_1000_1000_1000_1000_1000.to_chinese_numeral).to eq '千穣一千𥝱一千垓一千京一千兆一千億一千万一千'
+      expect(1_0000_0000_0000_0000_0000_0000_0000_0000.to_chinese_numeral).to eq '一溝'
+      expect(1000_1000_1000_1000_1000_1000_1000_1000_1000.to_chinese_numeral).to eq '千溝一千穣一千𥝱一千垓一千京一千兆一千億一千万一千'
+      expect(1_0000_0000_0000_0000_0000_0000_0000_0000_0000.to_chinese_numeral).to eq '一澗'
+      expect(1000_1000_1000_1000_1000_1000_1000_1000_1000_1000.to_chinese_numeral).to eq '千澗一千溝一千穣一千𥝱一千垓一千京一千兆一千億一千万一千'
+      expect(1_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000.to_chinese_numeral).to eq '一正'
+      expect(1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000.to_chinese_numeral).to eq '千正一千澗一千溝一千穣一千𥝱一千垓一千京一千兆一千億一千万一千'
+      expect(1_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000.to_chinese_numeral).to eq '一載'
+      expect(1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000.to_chinese_numeral).to eq '千載一千正一千澗一千溝一千穣一千𥝱一千垓一千京一千兆一千億一千万一千'
+      expect(1_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000.to_chinese_numeral).to eq '一極'
+      expect(1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000.to_chinese_numeral).to eq '千極一千載一千正一千澗一千溝一千穣一千𥝱一千垓一千京一千兆一千億一千万一千'
+      expect(1_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000.to_chinese_numeral).to eq '一恒河沙'
+      expect(1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000.to_chinese_numeral).to eq '千恒河沙一千極一千載一千正一千澗一千溝一千穣一千𥝱一千垓一千京一千兆一千億一千万一千'
+      expect(1_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000.to_chinese_numeral).to eq '一阿僧祇'
+      expect(1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000.to_chinese_numeral).to eq '千阿僧祇一千恒河沙一千極一千載一千正一千澗一千溝一千穣一千𥝱一千垓一千京一千兆一千億一千万一千'
+      expect(1_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000.to_chinese_numeral).to eq '一那由他'
+      expect(1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000.to_chinese_numeral).to eq '千那由他一千阿僧祇一千恒河沙一千極一千載一千正一千澗一千溝一千穣一千𥝱一千垓一千京一千兆一千億一千万一千'
+      expect(1_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000.to_chinese_numeral).to eq '一不可思議'
+      expect(1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000.to_chinese_numeral).to eq '千不可思議一千那由他一千阿僧祇一千恒河沙一千極一千載一千正一千澗一千溝一千穣一千𥝱一千垓一千京一千兆一千億一千万一千'
+      expect(1_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000.to_chinese_numeral).to eq '一無量大数'
+      expect(1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000_1000.to_chinese_numeral).to eq '千無量大数一千不可思議一千那由他一千阿僧祇一千恒河沙一千極一千載一千正一千澗一千溝一千穣一千𥝱一千垓一千京一千兆一千億一千万一千'
+    end
   end
 end
